078446ff07c74b0ed0e987115845cc22960cd725
Kirill Usenko <usenko.kirill2008@gmail.com>
2022-08-23 20:29:53 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Parallax, useParallax } from 'react-mouse-parallax'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const ref = useParallax({
+        bottom: 10,
+        right: 5,
+        type: 'px'
+    })
+
+    return (
+        <div className="Test">
+            <h1 style={{
+                textAlign: 'center'
+            }}>Parallax Example</h1>
+
+            <Parallax move={{
+                left: 5,
+                top: 4,
+                type: '%'
+            }}>
+                <img style={{
+                    borderRadius: '150px',
+                    marginLeft: '100px'
+                }} src="https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Контент для элемента с параллаксом" />
+            </Parallax>
+            
+            <img ref={ref} style={{
+                    margin: '100px 150px 150px 150px'
+                }} src="https://images.unsplash.com/photo-1499092346589-b9b6be3e94b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="Контент для элемента с параллаксом" />
+        </div>
+    )
 }
 
-export default App;
+export default App
